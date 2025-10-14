8737152c10f40b82403ab1dbb5e20dfa1d02779b
fabian <fabiandejesusfg@gmail.com>
2025-10-13 19:17:26 -0500

    primer commit

---
+++ src/main/java/com/example/SGI_API/SgiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SGI_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiApiApplication.class, args);
+	}
+
+}
